85fa28cca3b04c4bcd0cacdba9a33c1eec484af9
GitMaxx <126781549+GitMaxin@users.noreply.github.com>
2023-04-18 15:40:47 -0300

    Update MusicList.java

--- MusicList.java
+++ MusicList.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Playlist {
+public class MusicList {
     public static void main(String[] args) {
     ArrayList<String> listaParaIlhaDeserta = new ArrayList<String>();
     listaParaIlhaDeserta.add("Musica1");
